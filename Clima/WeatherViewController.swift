91d39876828dc499432084b18dbad726611a0d8e
Meelad Dawood <mdawood@sandiego.edu>
2017-12-29 18:30:20 -0800

    added updateUI function that updates the UI with the data from the weather model

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -88,6 +88,9 @@ class WeatherViewController: UIViewController , CLLocationManagerDelegate {
         weatherDataModel.condition = json["weather"][0]["id"].intValue
         
         weatherDataModel.weatherIconName = weatherDataModel.updateWeatherIcon(condition: weatherDataModel.condition)
+            
+        updateUIWithWeatherData()
+            
         } else {
             cityLabel.text = "Weather Unavaliable"
         }
@@ -102,6 +105,12 @@ class WeatherViewController: UIViewController , CLLocationManagerDelegate {
     
     
     //Write the updateUIWithWeatherData method here:
+    func updateUIWithWeatherData() {
+        cityLabel.text = weatherDataModel.city
+        temperatureLabel.text = String(weatherDataModel.temperature)
+        weatherIcon.image = UIImage(named: weatherDataModel.weatherIconName)
+        
+    }
     
     
     
